794d072bebddb26705a66596483c727fc8725c02
Orozquiano <xorozcox5000@gmail.com>
2021-09-20 01:29:57 -0500

    Validacion correo

--- src/main.js
+++ src/main.js
@@ -1,4 +1,38 @@
-var user = {
-    email: document.getElementById('inputEmail4'),
-    
+
+function Registro() {
+    const userV = {
+        email: document.getElementById('inputEmail4').value,
+        password: document.getElementById('inputPassword4').value,
+        confirmPassword: document.getElementById('inputPassword4_1').value,
+        address: document.getElementById('inputAddress').value,
+        // address2: document.getElementById('inputAddress2').value,
+        city: document.getElementById('inputCity').value,
+        state: document.getElementById('inputState').value,
+        zip: document.getElementById('inputZip').value
+    }
+    if(userV.email.length == 0 || userV.password.length == 0 || userV.confirmPassword.length == 0 || userV.address.length == 0 || userV.city.length == 0 || userV.state.length == 0 || userV.zip.length == 0){
+        alert("Llene todos los campos");
+    }else{
+        validation(userV.email,userV.password);
+    }
+}
+
+function validation(email,password){
+    //email
+    var expEmail= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido = expEmail.test(email);
+    if(esValido==true){
+        alert('El correo es valido');
+    }else{
+        alert('El correo no es valido');
+    }
+    //password
+    var expPass = /^(?=.*[!@#$%^&*])/;
+    var esvalidoP = expPass.test(password);
+    if(esvalidoP == true){
+        alert('Contraseña segura');
+    }else{
+        alert('Tu contraseña no es segura');
+    }
+
 }
