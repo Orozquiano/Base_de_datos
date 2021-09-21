bce2f3fe24796cb0c63023b08cbb66504a1c89e5
Orozquiano <xorozcox5000@gmail.com>
2021-09-20 19:57:46 -0500

    Tarea finalizada

--- src/main.js
+++ src/main.js
@@ -9,65 +9,56 @@ function Registro() {
         city: document.getElementById('inputCity').value,
         state: document.getElementById('inputState').value,
         zip: document.getElementById('inputZip').value,
-        img: document.getElementById('formFile').files
+        img: document.getElementById('formFile')
     }
-    if(userV.email.length == 0 || userV.password.length == 0 || userV.confirmPassword.length == 0 || userV.address.length == 0 || userV.city.length == 0 || userV.state.length == 0 || userV.zip.length == 0 || userV.img.length == 0){
-        // if(userV.file.length==0){
-            alert("Llene todos los campos");
-        // }else{
-        //     alert("Hay una imagen")
-        // }
-    }else{
-        if(userV.password == userV.confirmPassword){
-            validation(userV.email,userV.password);
-        }else{
+    if (userV.email.length == 0 || userV.password.length == 0 || userV.confirmPassword.length == 0 || userV.address.length == 0 || userV.city.length == 0 || userV.state.length == 0 || userV.zip.length == 0 || userV.img.files.length == 0) {
+        alert("Llene todos los campos");
+    } else {
+        if (userV.password == userV.confirmPassword) {
+            validation(userV.email, userV.password,userV.img);
+        } else {
             alert("Su contraseña no es igual")
         }
     }
 }
 
-function validation(email,password){
+function validation(email, password,img) {
     //email
-    var expEmail= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var expEmail = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
     var esValido = expEmail.test(email);
-    if(esValido==true){
+    if (esValido == true) {
         alert('El correo es valido');
-    }else{
+    } else {
         alert('El correo no es valido');
     }
     //password
     var expPass = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{7,}$/;
     var esvalidoP = expPass.test(password);
-    if(esvalidoP == true){
+    if (esvalidoP == true) {
         alert('Contraseña segura');
-    }else{
+    } else {
         alert('Tu contraseña no es segura');
     }
     //IMG
-//     var uploadFile = obj.files[0];
+    validateFile(img);
+}
 
-//     if (!window.FileReader) {
-//         alert('El navegador no soporta la lectura de archivos');
-//         return;
-//     }
+function validateFile(img) {
+    var allowedExtension = ['jpeg', 'jpg', 'png', 'gif'];
+    var fileExtension = img.value.split('.').pop().toLowerCase();
+    var isValidFile = false;
 
-//     if (!(/\.(jpg|png|gif)$/i).test(uploadFile.name)) {
-//         alert('El archivo a adjuntar no es una imagen');
-//     }
-//     else {
-//         var img = new Image();
-//         img.onload = function () {
-//             if (this.width.toFixed(0) != 200 && this.height.toFixed(0) != 200) {
-//                 alert('Las medidas deben ser: 200 * 200');
-//             }
-//             else if (uploadFile.size > 20000)
-//             {
-//                 alert('El peso de la imagen no puede exceder los 200kb')
-//             }
-//             else {
-//                 alert('Imagen correcta :)')                
-//             }
-//         };
-//         img.src = URL.createObjectURL(uploadFile);
-//     }                 
+    for (var index in allowedExtension) {
+
+        if (fileExtension === allowedExtension[index]) {
+            isValidFile = true;
+            break;
+        }
+    }
+
+    if (!isValidFile) {
+        alert('La imagen debe ser : *.' + allowedExtension.join(', *.'));
+    }
+
+    return isValidFile;
 }
